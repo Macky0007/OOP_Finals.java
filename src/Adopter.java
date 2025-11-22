f05c6d622d4ab4e406700eec9b8fc34b79dac607
darl-12 <darldimaculangan@gmail.com>
2025-11-22 17:25:35 +0800

    Update Adopter.java

--- src/Adopter.java
+++ src/Adopter.java
@@ -2,19 +2,21 @@ public class Adopter{
 
     public String adopterName;
     public int adopterAge;
+    public String adopterEmail;
     public String adopterContact;
     public Animal adoptedAnimal;
     private String adopterAddress;
 
-    public Adopter(String adopterName, int adopterAge,String adopterContact, Animal adoptedAnimal, String adopterAddress){
+    public Adopter(String adopterName, int adopterAge,String adopterContact, String adopterEmail, Animal adoptedAnimal, String adopterAddress){
         this.adopterName = adopterName;
         this.adopterAge = adopterAge;
         this.adopterContact = adopterContact;
+        this.adopterEmail = adopterEmail;
         this.adoptedAnimal = adoptedAnimal;
         this.adopterAddress = adopterAddress;
     }
 
     public String toString(){
-        return adopterName + "," + adopterAge + "," + adopterContact + "," + adoptedAnimal;
+        return adopterName + "," + adopterAge + "," + adopterContact + "," + adopterEmail + "," + adoptedAnimal;
     }
 }
