33e908f38e92fbc991a18ea8ed466a3315cf6e63
darl-12 <darldimaculangan@gmail.com>
2025-11-22 22:29:26 +0800

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,44 +1,42 @@
 public class Animal {
         public String name;
-        public String species;
         public int age;
         private String animalID;
         private boolean isMale = true;
 
-        public Animal(String name, String species, int age, String animalID, boolean isMale){
+        public Animal(String animalID, String name, int age, boolean isMale){
             this.name = name;
-            this.species = species;
             this.age = age;
             this.animalID = animalID;
             this.isMale = isMale;
         }
         public String toString(){
-            return species + "," + name + "," + age + "," + isMale;
+            return animalID+ "," + name + "," + age + "," + isMale;
         }
     }
 
     //Classes of different species of animal as an implementation of Inheritance
     class Dog extends Animal{
-        public Dog(String name, int age, String animalID, boolean isMale){
-            super(name, "Dog", age, animalID, isMale);
+        public Dog(String animalID, String name, int age, boolean isMale){
+            super(animalID, name, age, isMale);
         }
 
     }
 
     class Cat extends Animal{
-        public Cat(String name, int age, String animalID, boolean isMale){
-            super(name, "Cat", age, animalID, isMale);
+        public Cat(String animalID, String name, int age, boolean isMale){
+            super(animalID, name, age, isMale);
         }
     }
 
     class Hamster extends Animal{
-        public Hamster(String name, int age, String animalID, boolean isMale){
-            super(name, "Hamster", age, animalID, isMale);
+        public Hamster(String animalID, String name, int age, boolean isMale){
+            super(animalID, name, age, isMale);
         }
     }
 
     class Rabbit extends Animal{
-        public Rabbit(String name, int age, String animalID, boolean isMale){
-            super(name, "Rabbit", age, animalID, isMale);
+        public Rabbit(String animalID, String name, int age, boolean isMale){
+            super(animalID, name, age, isMale);
         }
 }
